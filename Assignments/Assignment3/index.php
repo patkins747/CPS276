541991200153df5e9fea6b2a46b4a2672aa11918
patkins747 <patkins@wccnet.edu>
2026-02-19 12:47:45 -0500

    upload

--- Assignments/Assignment3/index.php
+++ Assignments/Assignment3/index.php
@@ -2,10 +2,10 @@
 $output = "";
 
 if($_SERVER["REQUEST_METHOD"] == "POST"){
- require_once 'processNames.php'>
+ require_once 'processNames.php';
  $output = addClearNames();
 }
-}
+
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -19,11 +19,26 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
     <form class="row g-3" action="#" method="POST">
     <div class="row g-3">
         <h1>Add Names</h1>
+        <div class="col-12">
         <button type="submit" class="btn btn-primary" name="addName">Add Name</button>
         <button type="submit" class="btn btn-primary" name="clearNames">Clear Names</button>
+        </div>
         <label for="enterName" class="form-label">Enter Name</label>
         <input type="text" class="form-control" name="enterName" id="enterName">
     </div>
-    <textarea style="height: 500px;" class="form-control" id="namelist" name="namelist"><?php echo $output ?></textarea>
+    <label for="nameList" class="form-label">List of Names</label>
+    <textarea style="height: 500px;" class="form-control" id="nameList" name="nameList"><?php echo $output ?></textarea>
    </form>
 </body>
+<!--
+1. What is the purpose of separating the functionality between index.php and processNames.php in this assignment?
+
+2. How does the $_SERVER["REQUEST_METHOD"] variable help determine when to process form submissions in PHP?
+
+3. How does PHP handle string-to-array conversion using the explode function, and why is this useful in this application?
+
+4. What role does the implode function play in formatting the output for the textarea?
+
+5. How does processNames.php determine whether to add a new name or clear all names based on which button was clicked?
+
+-->
--- Assignments/Assignment3/processNames.php
+++ Assignments/Assignment3/processNames.php
@@ -1,3 +1,30 @@
 <?php
+function addClearNames(){
+    if ($_SERVER["REQUEST_METHOD"] == "POST") {
+        if (isset($_POST['clearNames'])) {
+            return "";
+        } elseif (isset($_POST['addName'])) {
+            if (isset($_POST['enterName'])){
+                $nameField = explode(' ',$_POST['enterName']);
+                $newName = $nameField[1].", ".$nameField[0];
+                $newName = ucwords($newName);
+                if(isset($_POST['nameList'])){
+                    $nameList = $_POST['nameList'];
+                    $nameArray = explode("\n", $nameList);
+                    array_push($nameArray, $newName);
+                    sort($nameArray,SORT_NATURAL);
+                    $nameList = implode("\n",$nameArray);
+                    
+                }
+                else{
+                    $nameList = $newName;
+                }
 
+        
+                return $nameList;
+            }        
+        }
+    }
+    return "";
+}
 ?>
